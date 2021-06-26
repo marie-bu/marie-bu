f76b5ede80cf085594761cd057ca574d91b781b7
Marie Burki <marie.buerki@gmail.com>
2021-06-26 11:15:48 +0200

    Career

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const landingSection = document.querySelector("#navigation");
 const landingBtns = landingSection.querySelectorAll(".btn-circle");
 const sections = document.querySelectorAll(".section");
 
+const careerTitles = document.querySelectorAll(".career-title");
+const careerTexts = document.querySelectorAll(".career-text");
+
 // Landing page navigation
 
 landingBtns.forEach((btn)=>{
@@ -13,6 +16,23 @@ landingBtns.forEach((btn)=>{
             if (section.id == btn.classList[0]) {
                 section.classList.add("section-active");
             }
-        })
-    })
-})
+        });
+    });
+});
+
+// Career 
+
+careerTitles.forEach((title) => {
+    title.addEventListener("click", () => {
+        careerTitles.forEach(t => t.classList.remove("career-title-active"));
+        careerTexts.forEach((text) => {
+            if (text.classList[0] === title.classList[0]) {
+                text.classList.remove("career-text-hidden");
+                title.classList.add("career-title-active");
+                console.log(title);
+            } else {
+                text.classList.add("career-text-hidden");
+            }
+        });
+    });
+});
