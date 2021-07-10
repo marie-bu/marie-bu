e827d6649d60b69f6e6ed51bc4d64007ad8a22d1
Marie Burki <marie.buerki@gmail.com>
2021-07-10 13:00:38 +0200

    navigation; home page and header

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 // DOM elements
 
-const landingSection = document.querySelector("#navigation");
-const landingBtns = landingSection.querySelectorAll(".btn-circle");
-const sections = document.querySelectorAll(".section");
+const lightModeBtn = document.querySelector(".btn-switch-light");
+const darkModeBtn = document.querySelector(".btn-switch-dark");
+const root = document.querySelector(":root");
+const contactMaltBtn = document.querySelector("#malt-btn");
+
+const nav = document.querySelector("nav");
+const navBtns = nav.querySelectorAll(".btn-icon-s");
+const header = document.querySelector("header");
+const home = document.querySelector("section#home");
+const homeBtns = home.querySelectorAll(".btn-circle");
+const sections = document.querySelectorAll("section");
 
 const careerTitles = document.querySelectorAll(".career-title");
 const careerTexts = document.querySelectorAll(".career-text");
@@ -10,14 +18,46 @@ const careerTexts = document.querySelectorAll(".career-text");
 const btnsMore = document.querySelectorAll(".btn-icon-more");
 const projects = document.querySelectorAll(".project");
 
-// Landing page navigation
+// light-mode 
 
-landingBtns.forEach((btn)=>{
+lightModeBtn.addEventListener("click", ()=>{
+    lightModeBtn.classList.add("active");
+    darkModeBtn.classList.remove("active");
+    root.classList.add("light-mode");
+    contactMaltBtn.classList.add("light-mode");
+});
+
+darkModeBtn.addEventListener("click", ()=>{
+    darkModeBtn.classList.add("active");
+    lightModeBtn.classList.remove("active");
+    root.classList.remove("light-mode");
+    contactMaltBtn.classList.remove("light-mode");
+})
+
+// Navigation
+
+navBtns.forEach((btn)=>{
     btn.addEventListener("click", ()=>{
-        landingSection.style.display = "none";
+        if (btn.classList[0] === "home")
+            header.classList.remove("active");
         sections.forEach((section)=>{
-            if (section.id == btn.classList[0]) {
-                section.classList.add("section-active");
+            if (section.id === btn.classList[0]) {
+                section.classList.add("active");
+            } else {
+                section.classList.remove("active");
+            }
+        });
+    });
+});
+
+homeBtns.forEach((btn)=>{
+    btn.addEventListener("click", ()=>{
+        header.classList.add("active");
+        sections.forEach((section)=>{
+            if (section.id === btn.classList[0]) {
+                section.classList.add("active");
+            } else {
+                section.classList.remove("active");
             }
         });
     });
@@ -27,14 +67,14 @@ landingBtns.forEach((btn)=>{
 
 careerTitles.forEach((title) => {
     title.addEventListener("click", () => {
-        careerTitles.forEach(t => t.classList.remove("career-title-active"));
+        careerTitles.forEach(t => t.classList.remove("active"));
         careerTexts.forEach((text) => {
             if (text.classList[0] === title.classList[0]) {
-                text.classList.remove("career-text-hidden");
-                title.classList.add("career-title-active");
+                text.classList.remove("hidden");
+                title.classList.add("active");
                 console.log(title);
             } else {
-                text.classList.add("career-text-hidden");
+                text.classList.add("hidden");
             }
         });
     });
@@ -47,15 +87,15 @@ projects.forEach((project)=> {
         let main = project.querySelector(".project-main");
         let details = project.querySelector(".project-details");
         console.log(main + details);
-        main.classList.add("project-main-hidden");
-        details.classList.add("project-details-active");
+        main.classList.add("hidden");
+        details.classList.add("active");
     })
     project.addEventListener("mouseout", ()=> {
         let main = project.querySelector(".project-main");
         let details = project.querySelector(".project-details");
         console.log(main + details);
-        main.classList.remove("project-main-hidden");
-        details.classList.remove("project-details-active");
+        main.classList.remove("hidden");
+        details.classList.remove("active");
     })
 })
 
@@ -63,8 +103,8 @@ btnsMore.forEach((btn) => {
     btn.addEventListener("click", () => {
         let main = btn.parentNode.previousElementSibling.previousElementSibling;
         let details = btn.parentNode.previousElementSibling;
-        main.classList.toggle("project-main-hidden");
-        details.classList.toggle("project-details-active");
+        main.classList.toggle("hidden");
+        details.classList.toggle("active");
         btn.blur();
     });
 });
